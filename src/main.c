853bd22e8bae087160415fdaefb3c7d7310e4371
Gabriel SCHWAB <gabriel.schwab2@gmail.com>
2021-12-21 17:43:13 +0100

    update .gitignore and main.c

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 
 
 
-#define MAX_SIZE 10 //maximum number of digits for each integer
+#define MAX_SIZE 10 //nombre maximal de chiffres pour un nombre
+#define MAX_BASE 16 //base maximale pour un nombre 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //argc est la base
 {   
 
     return 0;
